68763e355fd4d7b1539e2d5384afdda069dc2966
Diogo Mata Rodrigues <61853172+mat4rte@users.noreply.github.com>
2021-02-21 19:02:25 +0000

    Create isbn.py

---
+++ Treino 1/isbn.py
@@ -0,0 +1,27 @@
+'''
+Pretende-se que implemente uma função que detecte códigos ISBN inválidos. 
+Um código ISBN é constituído por 13 digitos, sendo o último um digito de controlo.
+Este digito de controlo é escolhido de tal forma que a soma de todos os digitos, 
+cada um multiplicado por um peso que é alternadamente 1 ou 3, seja um múltiplo de 10.
+A função recebe um dicionário que associa livros a ISBNs,
+e deverá devolver a lista ordenada de todos os livros com ISBNs inválidos.
+'''
+
+def isbn(livros):
+    
+    inv = []
+    for livro in livros:
+        sum1 = sum3 = 0
+        for idx, num in enumerate(livros[livro]):
+            
+            if idx == 0 or idx % 2 == 0:
+                sum1 = sum1 + int(num)*1
+                sum3 = sum3 + int(num)*3
+            else:
+                sum1+= int(num)*3
+                sum3 += int(num)*1
+                
+        if sum1 % 10 != 0 and sum3 % 10 != 0 :
+            inv.append(livro)
+        
+    return inv
