eb59a6178b209c097d1acdeefaf9c3e92a6b3564
Diogo Mata Rodrigues <61853172+mat4rte@users.noreply.github.com>
2021-02-24 16:09:15 +0000

    Update repete.py

--- Treino 1/repete.py
+++ Treino 1/repete.py
@@ -1,1 +1,17 @@
+'''
+Implemente uma função que determine qual a menor sequência de caracters que
+contém n repetições de uma determinada palavra
+'''
 
+def repete(palavra,n):
+    
+    seq = palavra
+    iguais = ""
+    for i in range(len(palavra)):
+        if palavra[i] == palavra[len(palavra) - i - 1]:
+            iguais+=palavra[i]
+            seq = seq[:-1]
+        else:
+            break
+    
+    return seq*n+iguais
